87b60cb69e6a8ebc9c6089fcbee7f9786294acac
Kasper Olsen <kasper@kols.dk>
2024-07-11 20:14:26 +0200

    progress

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const input = document.getElementById('input');
 const output = document.getElementById('output-area');
 const encode = document.getElementById('encode');
 const decode = document.getElementById('decode');
+const from = document.getElementById('from');
+const to = document.getElementById('to');
 const copyToClipboardButton = document.getElementById('copy-to-clipboard');
 const copyPrompt = document.getElementById("copy-prompt");
 const errorMessage = document.getElementById("error-message");
+
+const BASE64 = "Base64";
+const PLAIN = "Plain Text";
 
 function encodeData() {
     output.value = btoa(input.value);
@@ -36,10 +41,17 @@ decode.addEventListener('change', () => {
     decodeData();
 });
 
+from.innerText = BASE64;
+to.innerText = PLAIN;
+
 function swapInputOutput() {
     let temp = input.value;
     input.value = output.value;
     output.value = temp;
+
+    let tempText = from.innerText;
+    from.innerText = to.innerText;
+    to.innerText = tempText;
 }
 
 const copyButton = document.getElementById("copy-button");
